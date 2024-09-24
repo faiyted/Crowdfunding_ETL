1332db9ae8f31ce7e8ad57740bc87ff8a93d70de
faiyted <faiyted@gmai.com>
2024-09-23 21:06:02 -0500

    adding ERD

---
+++ crowdfunding_db.sql
@@ -0,0 +1,8 @@
+-- 1. Verify the data in the contacts table
+SELECT * FROM contacts;
+-- 2. Verify the data in the categories table
+SELECT * FROM category;
+-- 3. Verify the data in the subcategories table
+SELECT * FROM subcategory;
+-- 4. Verify the data in the campaigns table
+SELECT * FROM campaign;
